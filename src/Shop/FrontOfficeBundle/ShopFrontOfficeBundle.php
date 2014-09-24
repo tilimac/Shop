82fdb5261f1941dba6ab4d21340cf4110362e4af
Tilimac <Tilimac@Tilimac-PC>
2014-09-24 09:20:16 +0200

    Init

---
+++ src/Shop/FrontOfficeBundle/ShopFrontOfficeBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Shop\FrontOfficeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ShopFrontOfficeBundle extends Bundle
+{
+}
---
+++ src/Shop/UserBundle/ShopUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Shop\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ShopUserBundle extends Bundle
+{
+}
