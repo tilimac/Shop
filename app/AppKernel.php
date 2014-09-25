28aa031e5e6b12a3469f36cb4b9cffc7b1038a49
tilimac <tilimac@gmail.com>
2014-09-25 13:04:23 +0200

    Merge inscription

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Shop\FrontOfficeBundle\ShopFrontOfficeBundle(),
-            new Shop\UserBundle\ShopUserBundle(),
             new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
             new FOS\UserBundle\FOSUserBundle(),
         );
--- src/Shop/UserBundle/ShopUserBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Shop\UserBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class ShopUserBundle extends Bundle
-{
-}
